a355775dee53b3f88b57271f113fce7eacf85582
namnguyen1310 <17020910@vnu.edu.vn>
2020-04-30 15:59:04 +0700

    commit

---
+++ app/code/Shop/Rate/registration.php
@@ -0,0 +1,5 @@
+<?php 
+
+\Magento\Framework\Component\ComponentRegistrar::register( \Magento\Framework\Component\ComponentRegistrar::MODULE, 'Rate_Example',
+__DIR__
+);
---
+++ app/code/Shop/test/registration.php
@@ -0,0 +1,5 @@
+<?php 
+
+\Magento\Framework\Component\ComponentRegistrar::register( \Magento\Framework\Component\ComponentRegistrar::MODULE, 'Test_Example',
+__DIR__
+);
